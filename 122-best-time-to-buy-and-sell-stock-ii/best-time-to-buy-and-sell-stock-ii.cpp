b984868c29c879830474663304c9bc9789cd7a81
DEVA982 <67541946+DEVA982@users.noreply.github.com>
2026-08-04 15:49:36 +0530

    Time: 19 ms (5.21%) | Memory: 24.8 MB (30.28%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -16,7 +16,7 @@ public:
         
         if(buy==0){
             int not_sell = f(prices,ind+1,0,n,dp);
-            int sell = prices[ind]+f(prices,ind+1,1,n,dp);;
+            int sell = prices[ind]+f(prices,ind+1,1,n,dp);
             
             return dp[ind][buy]=max(sell,not_sell);
         }
@@ -27,7 +27,22 @@ public:
 
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return f(prices,0,1,n,dp);
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        for(int ind = n-1 ; ind>=0 ; ind--){
+            for (int buy = 0 ; buy<=1 ; buy++){
+                if(buy){
+                    int take = -prices[ind]+f(prices,ind+1,0,n,dp);
+                    int not_take = f(prices,ind+1,1,n,dp);
+                    dp[ind][buy]=max(take,not_take);
+                }
+                else{
+                    int not_sell = f(prices,ind+1,0,n,dp);
+                    int sell = prices[ind]+f(prices,ind+1,1,n,dp);
+                    
+                    dp[ind][buy]=max(sell,not_sell);
+                }
+            }
+        }
+        return dp[0][1];
     }
 };
