6a0a4f168a2f9d192f536f46366113b53f9020db
DEVA982 <67541946+DEVA982@users.noreply.github.com>
2026-09-14 16:06:41 +0530

    Time: 14 ms (55.90%) | Memory: 65.7 MB (44.58%) - LeetSync

--- 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
@@ -1,39 +1,32 @@
-#include<bits/stdc++.h>
-/**
-
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void inorder(TreeNode* root,vector<int> & ans){
-        if(root==nullptr){
-            return;
-        }
-        inorder(root->left,ans);
-        ans.push_back(root->val);
-        inorder(root->right,ans);
+
+    void inorder(TreeNode* root, vector<int>& v) {
+        if (!root) return;
+
+        inorder(root->left, v);
+        v.push_back(root->val);
+        inorder(root->right, v);
     }
-    TreeNode* insertTree(vector<int> ans,int low ,int high){
-        if (low > high)
-            return nullptr;
-        int mid = (low+high)/2;
-        TreeNode* root = new TreeNode(ans[mid]);
-        root->left=insertTree(ans,low,mid-1);
-        root->right=insertTree(ans,mid+1,high);
+
+    TreeNode* build(vector<int>& v, int l, int r) {
+        if (l > r) return nullptr;
+
+        int mid = l + (r - l) / 2;
+
+        TreeNode* root = new TreeNode(v[mid]);
+
+        root->left = build(v, l, mid - 1);
+        root->right = build(v, mid + 1, r);
+
         return root;
     }
+
     TreeNode* balanceBST(TreeNode* root) {
-        vector<int> ans;
-        inorder(root,ans);
-        return insertTree(ans,0,ans.size()-1);
-        
+        vector<int> v;
+
+        inorder(root, v);
+
+        return build(v, 0, v.size() - 1);
     }
 };
