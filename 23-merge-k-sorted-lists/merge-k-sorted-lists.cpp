2678d55b0db4009f3ce62cf374808cd19771c542
DEVA982 <67541946+DEVA982@users.noreply.github.com>
2026-09-16 23:09:33 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (39.97%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -10,36 +10,29 @@
  */
 class Solution {
 public:
-    ListNode* merge(ListNode* headA,ListNode* headB){
-        ListNode* dummy = new ListNode(0);
-        ListNode* temp = dummy;
-        while(headA!=nullptr && headB!=nullptr){
-            if(headA->val<headB->val){
-                temp->next=headA;
-                headA=headA->next;
-            }
-            else{
-                temp->next=headB;
-                headB=headB->next;
-            }
-            temp=temp->next;
-        }
-        if(headA==nullptr){
-            temp->next=headB;
-        }
-        else{
-            temp->next=headA;
-        }
-        return dummy->next;
-    }
+    
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         int size=lists.size();
         if(size==0) return nullptr;
         if(size==1) return lists[0];
-        ListNode* right = lists[0];
-        for(int i = 1 ; i<size ; i++){
-            right = merge(right,lists[i]);
+        priority_queue <pair<int,ListNode*>,vector<pair<int,ListNode*>>,greater<pair<int,ListNode*>>> pq;
+        for(int i = 0 ; i<size ; i++){
+            if(lists[i]!=nullptr){
+                pq.push({lists[i]->val,lists[i]});
+            }
         }
-        return right;
+        
+        ListNode* dummy = new ListNode(0);
+        ListNode* temp = dummy;
+        while(!pq.empty()){
+            ListNode* minNode = pq.top().second;
+            pq.pop();
+            if(minNode->next!=nullptr){
+                pq.push({minNode->next->val,minNode->next});
+            }
+            temp->next=minNode;
+            temp=temp->next;
+        }
+        return dummy->next;
     }
 };
