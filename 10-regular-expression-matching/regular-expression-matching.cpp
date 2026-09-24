475e2dbbfa522809f5257bc6b6d5668f030fb0d5
DEVA982 <67541946+DEVA982@users.noreply.github.com>
2026-09-25 00:32:27 +0530

    Time: 1 ms (64.31%) | Memory: 9.7 MB (11.02%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
 
+    vector<vector<int>> dp;
+
     bool solve(string &s, string &p, int i, int j) {
+
+        // Already calculated
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
         // Pattern finished
         if (j == p.size()) {
-            return i == s.size();
+            return dp[i][j] = (i == s.size());
         }
 
-        // Is current character matching?
+        // Does current character match?
         bool firstMatch = false;
 
         if (i < s.size() &&
@@ -16,25 +22,29 @@ public:
             firstMatch = true;
         }
 
-        // Next character is '*'
+        // Next pattern character is *
         if (j + 1 < p.size() && p[j + 1] == '*') {
 
-            // Option 1: use zero occurrences
+            // Option 1: zero occurrences
             bool skip = solve(s, p, i, j + 2);
 
-            // Option 2: use one/more occurrences
+            // Option 2: use current character
             bool take = firstMatch &&
                         solve(s, p, i + 1, j);
 
-            return skip || take;
+            return dp[i][j] = skip || take;
         }
 
-        // Normal character / '.'
-        return firstMatch &&
-               solve(s, p, i + 1, j + 1);
+        // Normal character or '.'
+        return dp[i][j] =
+            firstMatch && solve(s, p, i + 1, j + 1);
     }
 
     bool isMatch(string s, string p) {
+
+        dp.assign(s.size() + 1,
+                  vector<int>(p.size() + 1, -1));
+
         return solve(s, p, 0, 0);
     }
 };
